02e9cd42f41e849eb3a2f574a940910b26523976
Ulysse GIMENEZ <ulysse.gimenez@bioserenity.com>
2018-05-02 17:56:58 +0200

    .

---
+++ neuroPipelines/visBrain_SLEEP/Installation.py
@@ -0,0 +1,20 @@
+# 1_Installation of VisPy
+#
+# pip install pyqt
+# pip install vispy
+# pip install moto
+
+import vispy
+vispy.test()
+
+
+# 2_Installation of visBrain package
+# pip install pyqt5
+# pip install visbrain
+
+
+# 3_Clone package to be in developper mode
+# git clone https://github.com/EtienneCmb/visbrain.git visbrain/
+# cd visbrain/
+# pip install -r requirements.txt
+# python setup.py develop
